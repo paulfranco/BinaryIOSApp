b2ed8eb4a47d35c8675460d6e8b32b208f368e12
Paul Franco <paulfranco@me.com>
2018-05-17 20:03:18 -0700

    First Commit of Binary App

--- Binary/ViewController.swift
+++ Binary/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueEntryTextField: UITextField!
+    
+    @IBOutlet weak var binaryBtn: UIButton!
+    
+    @IBOutlet weak var decimalBtn: UIButton!
+    
+    let placeholder = NSAttributedString(string: "Enter a value...", attributes: [NSAttributedStringKey.foregroundColor:#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), NSAttributedStringKey.font: UIFont(name:"Menlo", size:36.0)!])
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        valueEntryTextField.attributedPlaceholder = placeholder
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func binaryBtnWasPressed(_ sender: Any) {
     }
-
+    
+    @IBAction func decimalBtnWasPressed(_ sender: Any) {
+    }
+    
 
 }
 
