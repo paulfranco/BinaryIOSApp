358ab99e6f0ac256e456ffaeb58f4cdaa4d50815
Paul Franco <paulfranco@me.com>
2018-05-17 18:52:27 -0700

    Initial Commit

---
+++ Binary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Binary
+//
+//  Created by Paul Franco on 5/17/18.
+//  Copyright © 2018 Paul Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
